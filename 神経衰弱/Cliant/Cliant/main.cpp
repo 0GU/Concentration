175dd0ea43c822af01d1269da74c0971bda172e8
AnjuzzZZ <56670053+AnjuzzZZ@users.noreply.github.com>
2021-06-10 11:21:33 +0900

    imagedata読み込み

--- 神経衰弱/Cliant/Cliant/main.cpp
+++ 神経衰弱/Cliant/Cliant/main.cpp
@@ -22,7 +22,70 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE,
 	SetWindowText("神経衰弱");
 
 	//画像読み込み
-	
+	int img[4][13] = {
+		{
+		   LoadGraph("image\\トランプ素材\\clover\\clover1.png"),
+		   LoadGraph("image\\トランプ素材\\clover\\clover2.png"),
+		   LoadGraph("image\\トランプ素材\\clover\\clover3.png"),
+		   LoadGraph("image\\トランプ素材\\clover\\clover4.png"),
+		   LoadGraph("image\\トランプ素材\\clover\\clover5.png"),
+		   LoadGraph("image\\トランプ素材\\clover\\clover6.png"),
+		   LoadGraph("image\\トランプ素材\\clover\\clover7.png"),
+		   LoadGraph("image\\トランプ素材\\clover\\clover8.png"),
+		   LoadGraph("image\\トランプ素材\\clover\\clover9.png"),
+		   LoadGraph("image\\トランプ素材\\clover\\clover10.png"),
+		   LoadGraph("image\\トランプ素材\\clover\\clover11.png"),
+		   LoadGraph("image\\トランプ素材\\clover\\clover12.png"),
+		   LoadGraph("image\\トランプ素材\\clover\\clover13.png")
+		},
+		{
+		   LoadGraph("image\\トランプ素材\\spade\\spade1.png"),
+		   LoadGraph("image\\トランプ素材\\spade\\spade2.png"),
+		   LoadGraph("image\\トランプ素材\\spade\\spade3.png"),
+		   LoadGraph("image\\トランプ素材\\spade\\spade4.png"),
+		   LoadGraph("image\\トランプ素材\\spade\\spade5.png"),
+		   LoadGraph("image\\トランプ素材\\spade\\spade6.png"),
+		   LoadGraph("image\\トランプ素材\\spade\\spade7.png"),
+		   LoadGraph("image\\トランプ素材\\spade\\spade8.png"),
+		   LoadGraph("image\\トランプ素材\\spade\\spade9.png"),
+		   LoadGraph("image\\トランプ素材\\spade\\spade10.png"),
+		   LoadGraph("image\\トランプ素材\\spade\\spade11.png"),
+		   LoadGraph("image\\トランプ素材\\spade\\spade12.png"),
+		   LoadGraph("image\\トランプ素材\\spade\\spade13.png")
+		},
+		{
+		   LoadGraph("image\\トランプ素材\\dia\\dia1.png"),
+		   LoadGraph("image\\トランプ素材\\dia\\dia2.png"),
+		   LoadGraph("image\\トランプ素材\\dia\\dia3.png"),
+		   LoadGraph("image\\トランプ素材\\dia\\dia4.png"),
+		   LoadGraph("image\\トランプ素材\\dia\\dia5.png"),
+		   LoadGraph("image\\トランプ素材\\dia\\dia6.png"),
+		   LoadGraph("image\\トランプ素材\\dia\\dia7.png"),
+		   LoadGraph("image\\トランプ素材\\dia\\dia8.png"),
+		   LoadGraph("image\\トランプ素材\\dia\\dia9.png"),
+		   LoadGraph("image\\トランプ素材\\dia\\dia10.png"),
+		   LoadGraph("image\\トランプ素材\\dia\\dia11.png"),
+		   LoadGraph("image\\トランプ素材\\dia\\dia12.png"),
+		   LoadGraph("image\\トランプ素材\\dia\\dia13.png")
+		},
+		{
+		   LoadGraph("image\\トランプ素材\\heart\\heart1.png"),
+		   LoadGraph("image\\トランプ素材\\heart\\heart2.png"),
+		   LoadGraph("image\\トランプ素材\\heart\\heart3.png"),
+		   LoadGraph("image\\トランプ素材\\heart\\heart4.png"),
+		   LoadGraph("image\\トランプ素材\\heart\\heart5.png"),
+		   LoadGraph("image\\トランプ素材\\heart\\heart6.png"),
+		   LoadGraph("image\\トランプ素材\\heart\\heart7.png"),
+		   LoadGraph("image\\トランプ素材\\heart\\heart8.png"),
+		   LoadGraph("image\\トランプ素材\\heart\\heart9.png"),
+		   LoadGraph("image\\トランプ素材\\heart\\heart10.png"),
+		   LoadGraph("image\\トランプ素材\\heart\\heart11.png"),
+		   LoadGraph("image\\トランプ素材\\heart\\heart12.png"),
+		   LoadGraph("image\\トランプ素材\\heart\\heart13.png")
+		}
+
+	};
+
 	//送受信データ処理用
 	char StrBuf[256] = { "null" };//256バイトまで
 
